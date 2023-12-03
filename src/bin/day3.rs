52db3dd9452d8081847fd73dd7afb27042438148
Sebastian Holmin <sebastian.holmin@mullvad.net>
2023-12-03 15:28:28 +0100

    Solve day3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,22 +6,14 @@ use itertools::Itertools;
 
 fn main() {
     let input = std::fs::read_to_string("input/day3.txt").unwrap();
-    println!("Answer 1: {}", solve(&input));
-    // println!("Answer 2: {}", solve_2(&input));
+    // println!("Answer 1: {}", solve(&input));
+    println!("Answer 2: {}", solve_2(&input));
 }
-
-//  521242 too low
-
-// line 6
-// 1 at 35,6 is not a part
-// 4 at 29,6 is not a part
 
 fn solve(input: &str) -> usize {
     let mut symbols = HashMap::new();
     for (y, line) in input.lines().enumerate() {
-        for (x, symbol) in line.match_indices(|c: char| !c.is_numeric() && c != '.')
-        // .split(|c: char| !c.is_numeric())
-        {
+        for (x, symbol) in line.match_indices(|c: char| !c.is_numeric() && c != '.') {
             // println!("{:?}", (x, y, symbol));
             if let Some(double) = symbols.insert((x, y), symbol) {
                 panic!("{double}")
@@ -31,27 +23,112 @@ fn solve(input: &str) -> usize {
     let mut sum = 0;
     for (y, line) in input.lines().enumerate() {
         println!("line {y}");
-        for (start_x, c) in line.char_indices().filter(|(start_x, c)| !c.is_numeric()) {
-            // dbg!(num);
-            let start_x = line.find(num).unwrap();
+        let mut nums_with_pos: Vec<(usize, String)> = vec![];
+        let mut previous_char_was_num = false;
+        for (x, c) in line.char_indices() {
+            if !c.is_numeric() {
+                previous_char_was_num = false;
+                continue;
+            }
+            if !previous_char_was_num {
+                nums_with_pos.push((x, String::from(c)));
+                previous_char_was_num = true;
+            } else {
+                nums_with_pos.last_mut().unwrap().1.push(c);
+            }
+        }
+        for (start_x, num) in nums_with_pos {
             let left_right = [(start_x.saturating_sub(1), y), (start_x + num.len(), y)];
 
             let line = (start_x.saturating_sub(1)..start_x + num.len() + 1);
-            // dbg!(line.clone().collect_vec());
             let above = line.clone().map(|x| (x, y.saturating_sub(1)));
             let below = line.clone().map(|x| (x, y + 1));
 
             let mut surroundings = above.chain(left_right).chain(below);
 
-            if !surroundings.clone().any(|pos| symbols.contains_key(&pos)) {
-                dbg!(surroundings.collect_vec());
-                println!("{num} at {start_x},{y} is not a part");
+            if surroundings.any(|pos| symbols.contains_key(&pos)) {
                 sum += num.parse::<usize>().unwrap();
+            } else {
+                println!("{num} at {start_x},{y} is not a part");
             }
         }
     }
 
     sum
+}
+
+fn solve_2(input: &str) -> usize {
+    let mut gears: HashMap<(usize, usize), Vec<usize>> = HashMap::new();
+    for (y, line) in input.lines().enumerate() {
+        for (x, _symbol) in line.match_indices('*') {
+            // println!("{:?}", (x, y, symbol));
+            if let Some(double) = gears.insert((x, y), vec![]) {
+                panic!("{double:?}")
+            }
+        }
+    }
+
+    for (y, line) in input.lines().enumerate() {
+        println!("line {y}");
+        let mut nums_with_pos: Vec<(usize, String)> = vec![];
+        let mut previous_char_was_num = false;
+        for (x, c) in line.char_indices() {
+            if !c.is_numeric() {
+                previous_char_was_num = false;
+                continue;
+            }
+            if !previous_char_was_num {
+                nums_with_pos.push((x, String::from(c)));
+                previous_char_was_num = true;
+            } else {
+                nums_with_pos.last_mut().unwrap().1.push(c);
+            }
+        }
+
+        for (start_x, num) in nums_with_pos {
+            let left_right = [(start_x.saturating_sub(1), y), (start_x + num.len(), y)];
+
+            let line = (start_x.saturating_sub(1)..start_x + num.len() + 1);
+            let above = line.clone().map(|x| (x, y.saturating_sub(1)));
+            let below = line.clone().map(|x| (x, y + 1));
+
+            let mut surroundings = above.chain(left_right).chain(below);
+
+            for pos in surroundings {
+                if let Some(gear_nums) = gears.get_mut(&pos) {
+                    gear_nums.push(num.parse().unwrap());
+                    println!("* at {},{} is has num {num}", pos.0, pos.1);
+                    break;
+                }
+            }
+        }
+    }
+
+    gears
+        .iter()
+        .filter_map(|(_pox, nums)| {
+            if nums.len() == 2 {
+                Some(nums[0] * nums[1])
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+#[test]
+fn test_example_2() {
+    let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+    assert_eq!(solve_2(input), 467835)
 }
 
 #[test]
