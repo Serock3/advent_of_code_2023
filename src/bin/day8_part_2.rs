25f8114061e07173ad4a63d71271842c03e8d875
Sebastian Holmin <sebastian.holmin@mullvad.net>
2023-12-08 11:50:49 +0100

    Add Num crate and optimize dep builds

--- src/bin/day8_part_2.rs
+++ src/bin/day8_part_2.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use itertools::Itertools;
+use num::integer::lcm;
 use std::collections::HashMap;
 
 fn main() {
@@ -45,24 +46,7 @@ fn solve(input: &str) -> usize {
         })
         .collect_vec();
 
-    lcm(&loop_len)
-}
-
-// Courtesy of https://github.com/TheAlgorithms/Rust/blob/master/src/math/lcm_of_n_numbers.rs
-pub fn lcm(nums: &[usize]) -> usize {
-    if nums.len() == 1 {
-        return nums[0];
-    }
-    let a = nums[0];
-    let b = lcm(&nums[1..]);
-    a * b / gcd_of_two_numbers(a, b)
-}
-
-fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
-    if b == 0 {
-        return a;
-    }
-    gcd_of_two_numbers(b, a % b)
+    loop_len.into_iter().reduce(lcm).unwrap()
 }
 
 #[test]
