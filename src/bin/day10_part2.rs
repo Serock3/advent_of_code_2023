1b26c1cdd175b9582c6a26e210742a4f8da9ff87
Sebastian <sebastian.holmin@hotmail.com>
2023-12-10 14:49:07 +0100

    Refactor part 2

--- src/bin/day10_part2.rs
+++ src/bin/day10_part2.rs
@@ -18,7 +18,10 @@ enum Direction {
     West,
 }
 use Direction::*;
-impl From<&Direction> for (i32, i32) {
+
+type Pos = (i32, i32);
+
+impl From<&Direction> for Pos {
     fn from(value: &Direction) -> Self {
         match value {
             Direction::North => (-1, 0),
@@ -49,111 +52,106 @@ fn solve(input: &str) -> usize {
 
     let start_dirs = [North, East, South, West];
     let mut longest = 0;
-    let (_len, dir, area) = start_dirs
+    let area = start_dirs
         .into_iter()
-        .filter_map(|dir| {
+        .find_map(|dir| {
             follow_pipe(
                 (s_row.try_into().unwrap(), s_col.try_into().unwrap()),
                 dir,
                 &char_matrix,
             )
         })
-        .max_by(|(len_x, _, _), (len_y, _, _)| len_x.cmp(len_y))
         .unwrap();
 
     area.try_into().unwrap()
 }
 
-fn follow_pipe(
-    mut pos: (i32, i32),
-    mut dir: Direction,
-    char_matrix: &[Vec<char>],
-) -> Option<(usize, Direction, u32)> {
+fn follow_pipe(mut pos: Pos, mut dir: Direction, char_matrix: &[Vec<char>]) -> Option<u32> {
     let start_dir = dir.clone();
-    let mut steps = 0;
 
-    let mut twice_area = 0;
-    let mut num_straight = 0;
-    let mut num_bends = 0;
+    // Twice the area enclosed by the looping pipes
+    let mut twice_signed_enclosed_erea = 0;
+    // To accound for the thickness of the pipes themselves,
+    // we must count the number of straight and bent pieces.
+    let mut num_straight_pipes = 0;
+    let mut num_bent_pipes = 0;
+
     loop {
-        // Take step
-        steps += 1;
+        twice_signed_enclosed_erea += twise_area_change(pos, &dir);
 
-        twice_area += twise_area_change(pos, &dir);
-
-        pos = add_pos(pos, &dir);
+        pos = step(pos, &dir);
         let pipe = get_pipe(pos, char_matrix)?;
+
         match pipe {
-            '|' => num_straight += 1,
-            '-' => num_straight += 1,
-            'L' => num_bends += 1,
-            'J' => num_bends += 1,
-            '7' => num_bends += 1,
-            'F' => num_bends += 1,
+            '|' => num_straight_pipes += 1,
+            '-' => num_straight_pipes += 1,
+            // We have to compare the end direction to the start direction
+            // to know if the 'S' represents a straight or curved pipe
             'S' => {
                 if dir == start_dir {
-                    num_straight += 1
+                    num_straight_pipes += 1
                 } else {
-                    num_bends += 1
+                    num_bent_pipes += 1
                 }
             }
-            _ => {}
+            // Remaining pipe types are curved
+            _ => num_bent_pipes += 1,
         }
+
         dir = match (dir, pipe) {
-            (dir, 'S') => {
-                return Some((
-                    steps,
-                    dir,
-                    get_gap_area(
-                        (twice_area.abs() / 2).try_into().unwrap(),
-                        num_straight,
-                        num_bends,
-                    ),
-                ))
-            }
-            // North
             (North, '|') => North,
             (North, 'F') => East,
             (North, '7') => West,
-            // East
             (East, '-') => East,
             (East, '7') => South,
             (East, 'J') => North,
-            // South
             (South, '|') => South,
             (South, 'J') => West,
             (South, 'L') => East,
-            // West
             (West, '-') => West,
             (West, 'L') => North,
             (West, 'F') => South,
-
-            (dir, sym) => {
-                // dbg!((dir, sym));
-                return None;
+            (dir, 'S') => {
+                return Some(get_gap_area(
+                    twice_signed_enclosed_erea / 2,
+                    num_straight_pipes,
+                    num_bent_pipes,
+                ))
             }
+            // Hit a stop
+            _ => return None,
         };
     }
 }
 
-fn get_gap_area(enclosed_area: u32, num_straight: u32, num_bends: u32) -> u32 {
-    let num_extra_bends = num_bends - 4;
-    enclosed_area - (num_straight + num_extra_bends) / 2 - 1
+/// Get the total enclosed area by the pipes, encounting for their own thickness.
+///
+/// Each pipe piece has an area of 1. For a straight piece, half of that area is always
+/// inside the curve. For a bent piece, it depends on whether it is concave of convex.
+fn get_gap_area(signed_enclosed_area: i32, num_straight: u32, num_bends: u32) -> u32 {
+    let unsigned_area: u32 = signed_enclosed_area.abs().try_into().unwrap();
+    unsigned_area - (num_straight + (num_bends - 4)) / 2 - 1
 }
 
-fn twise_area_change(pos: (i32, i32), dir: &Direction) -> i32 {
-    let (dr, dc) = <(i32, i32)>::from(dir);
+/// Calculate the difference in enclosed area made by line element according
+/// to Stoke's/Green's theorem. This will not include the thickness of the pipes,
+/// which is corrected for by [`get_gap_area`].
+///
+/// See https://en.wikipedia.org/wiki/Green%27s_theorem#Area_calculation
+fn twise_area_change(pos: Pos, dir: &Direction) -> i32 {
+    let (dr, dc) = <Pos>::from(dir);
     let (dx, dy) = (dc, -dr);
     let (x, y) = (pos.1, -pos.0);
     x * dy - y * dx
 }
 
-fn add_pos(s_pos: (i32, i32), dir: &Direction) -> (i32, i32) {
-    let (r, c) = <(i32, i32)>::from(dir);
+fn step(s_pos: Pos, dir: &Direction) -> Pos {
+    let (r, c) = <Pos>::from(dir);
     (s_pos.0 + r, s_pos.1 + c)
 }
 
-fn get_pipe(pos: (i32, i32), char_matrix: &[Vec<char>]) -> Option<char> {
+/// Indexes into char matrix and gets pipe. Filters '.' chars.
+fn get_pipe(pos: Pos, char_matrix: &[Vec<char>]) -> Option<char> {
     let row = usize::try_from(pos.0).ok()?;
     let col = usize::try_from(pos.1).ok()?;
 
@@ -164,19 +162,24 @@ fn get_pipe(pos: (i32, i32), char_matrix: &[Vec<char>]) -> Option<char> {
         .filter(|char| *char != '.')
 }
 
-#[test]
-fn test_gap_area() {
-    assert_eq!(get_gap_area(10, 6, 10), 3)
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_gap_area2() {
-    assert_eq!(get_gap_area(9, 8, 8), 2)
-}
+    // Hand made test for by gap area fn
+    #[test]
+    pub(crate) fn test_gap_area() {
+        assert_eq!(get_gap_area(10, 6, 10), 3)
+    }
 
-#[test]
-fn test_example() {
-    let input = "...........
+    #[test]
+    pub(crate) fn test_gap_area2() {
+        assert_eq!(get_gap_area(9, 8, 8), 2)
+    }
+
+    #[test]
+    pub(crate) fn test_example() {
+        let input = "...........
 .S-------7.
 .|F-----7|.
 .||.....||.
@@ -185,12 +188,12 @@ fn test_example() {
 .|..|.|..|.
 .L--J.L--J.
 ...........";
-    assert_eq!(solve(input), 4)
-}
+        assert_eq!(solve(input), 4)
+    }
 
-#[test]
-fn test_example2() {
-    let input = ".F----7F7F7F7F-7....
+    #[test]
+    pub(crate) fn test_example2() {
+        let input = ".F----7F7F7F7F-7....
 .|F--7||||||||FJ....
 .||.FJ||||||||L7....
 FJL7L7LJLJ||LJ.L-7..
@@ -200,15 +203,16 @@ L--J.L7...LJS7F-7L7.
 .....|FJLJ|FJ|F7|.LJ
 ....FJL-7.||.||||...
 ....L---J.LJ.LJLJ...";
-    assert_eq!(solve(input), 8)
-}
+        assert_eq!(solve(input), 8)
+    }
 
-#[test]
-fn test_example3() {
-    let input = ".....
+    #[test]
+    pub(crate) fn test_example3() {
+        let input = ".....
 .S-7.
 .|.|.
 .L-J.
 .....";
-    assert_eq!(solve(input), 1)
+        assert_eq!(solve(input), 1)
+    }
 }
