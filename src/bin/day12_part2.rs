38e453d0a9d38408c8fb53edff8b13a7409b2a10
Sebastian <sebastian.holmin@hotmail.com>
2023-12-12 21:22:44 +0100

    WIP cache part 2

---
+++ src/bin/day12_part2.rs
@@ -0,0 +1,206 @@
+#![allow(unused)]
+use std::{collections::HashMap, fmt::format, sync::Mutex};
+
+use itertools::Itertools;
+use rayon::prelude::*;
+
+fn main() {
+    let input = std::fs::read_to_string("input/day12.txt").unwrap();
+    println!("Answer: {}", solve(&input));
+}
+
+fn solve(input: &str) -> usize {
+    let mut counter = Mutex::new(0);
+    let lines = input.lines().collect_vec();
+    let num_lines = lines.len();
+    lines
+        .into_par_iter()
+        .map(|line| {
+            let res = solve_times_five(line);
+            let mut counter = counter.lock().unwrap();
+            *counter += 1;
+            println!("{}/{} Done with {line}", *counter + 1, num_lines);
+
+            res
+        })
+        .sum()
+}
+
+fn solve_single(input: &str) -> usize {
+    let (condition, sizes) = input.split_once(' ').unwrap();
+    let sizes = sizes
+        .split(',')
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect_vec();
+
+    let mut cache = HashMap::new();
+    solve_recursive(&sizes, &condition.chars().collect_vec(), &mut cache)
+}
+
+fn solve_times_five(input: &str) -> usize {
+    let (condition, sizes) = input.split_once(' ').unwrap();
+    let sizes = sizes
+        .split(',')
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect_vec();
+    let sizes = sizes.repeat(5);
+
+    let condition = std::iter::repeat(condition).take(5).join("?");
+    let mut cache = HashMap::new();
+    solve_recursive(&sizes, &condition.chars().collect_vec(), &mut cache)
+}
+
+fn solve_recursive(
+    sizes: &[usize],
+    conditions: &[char],
+    cache: &mut HashMap<(&[usize], &[char]), usize>,
+) -> usize {
+    if sizes.is_empty() {
+        return !conditions.contains(&'#') as usize;
+    }
+    if !(conditions.contains(&'#') || conditions.contains(&'?')) {
+        return 0;
+    }
+    if conditions.len() < sizes.iter().sum::<usize>() + sizes.len() - 1 {
+        return 0;
+    }
+
+    if let Some(chached_ret) = cache.get(&(sizes, conditions)) {
+        return *chached_ret;
+    }
+
+    let (current_size, remaining_sizes) = sizes.split_first().unwrap();
+    let ret = match conditions[0] {
+        '.' => solve_recursive(sizes, &conditions[1..], cache),
+        '#' => {
+            if conditions[..*current_size].contains(&'.') {
+                0
+            } else {
+                match conditions.get(*current_size) {
+                    Some('#') => 0, // Not valid if next char is #
+                    None => remaining_sizes.is_empty() as usize,
+                    _ => solve_recursive(remaining_sizes, &conditions[1 + *current_size..], cache),
+                }
+            }
+        }
+        _ => {
+            match conditions.get(*current_size) {
+                Some('#') => {
+                    // Not a valid spot, move right
+                    solve_recursive(sizes, &conditions[1..], cache)
+                }
+                None => {
+                    // Last possible pos,
+                    (!conditions[..*current_size].contains(&'.') && remaining_sizes.is_empty())
+                        as usize
+                }
+                _ => {
+                    if let Some(i) = conditions[..*current_size].iter().position(|c| c == &'.') {
+                        // Not a valid spot, step until '.' is outside the interval
+                        solve_recursive(sizes, &conditions[i + 1..], cache)
+                    } else {
+                        let configs_for_this_pos = solve_recursive(
+                            remaining_sizes,
+                            &conditions[1 + *current_size..],
+                            cache,
+                        );
+
+                        // Step forward
+                        let configs_for_rest = solve_recursive(sizes, &conditions[1..], cache);
+
+                        configs_for_this_pos + configs_for_rest
+                    }
+                }
+            }
+        }
+    };
+    cache.insert((sizes, conditions), ret);
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub(crate) fn test_example_single() {
+        let input = "???.### 1,1,3";
+        assert_eq!(solve_times_five(input), 1)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single2() {
+        let input = ".??..??...?##. 1,1,3";
+        assert_eq!(solve_times_five(input), 16384)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single3() {
+        let input = "?#?#?#?#?#?#?#? 1,3,1,6";
+        assert_eq!(solve_times_five(input), 1)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single4() {
+        let input = "????.#...#... 4,1,1";
+        assert_eq!(solve_times_five(input), 16)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single5() {
+        let input = "????.######..#####. 1,6,5";
+        assert_eq!(solve_times_five(input), 2500)
+    }
+    #[test]
+    pub(crate) fn test_example_single6() {
+        let input = "?###???????? 3,2,1";
+        assert_eq!(solve_times_five(input), 506250)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single_custom1() {
+        let input = "??#.?#?#??? 1,3,1";
+        assert_eq!(solve_single(input), 2)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single_custom2() {
+        let input = "?.#?#??#?# 1,6";
+        assert_eq!(solve_single(input), 1)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single_custom3() {
+        let input = ".??#???.??? 3,1,1";
+        assert_eq!(solve_single(input), 12)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single_custom4() {
+        let input = "??##?#?????.. 5,1";
+        assert_eq!(solve_single(input), 7)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single_custom5() {
+        let input = "?#?#?????. 1,1,2";
+        assert_eq!(solve_single(input), 3)
+    }
+
+    #[test]
+    pub(crate) fn test_example_single_custom6() {
+        let input = "??.??#.??#?? 1,3,2,1";
+        assert_eq!(solve_single(input), 2)
+    }
+
+    #[test]
+    pub(crate) fn test_example() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        assert_eq!(solve(input), 525152)
+    }
+}
