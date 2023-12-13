15141fbfb8a824f280713dc1185eca4b1910f569
Sebastian <sebastian.holmin@hotmail.com>
2023-12-14 00:04:56 +0100

    Add ndarray

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 use std::{iter::Rev, slice::Iter};
 
-use advent_of_code::get_input;
+use advent_of_code::{get_input, transpose};
 use itertools::Itertools;
 
 fn main() {
@@ -43,20 +43,6 @@ fn reflection_at(row: usize, matrix: &[Vec<char>]) -> bool {
         .rev()
         .zip(lower)
         .all(|(reflected_line, line)| reflected_line == line)
-}
-
-fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
-    assert!(!v.is_empty());
-    let len = v[0].len();
-    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
-    (0..len)
-        .map(|_| {
-            iters
-                .iter_mut()
-                .map(|n| n.next().unwrap())
-                .collect::<Vec<T>>()
-        })
-        .collect()
 }
 
 #[test]
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use ndarray::prelude::*;
 use std::panic::Location;
 
 #[track_caller]
@@ -11,4 +12,25 @@ pub fn get_input() -> String {
         .0;
     let input_path = format!("input/day{n}.txt");
     std::fs::read_to_string(input_path).unwrap()
+}
+
+pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
+
+pub fn parse_char_matrix(input: &str) -> Array2<char> {
+    let shape = (input.lines().count(), input.lines().next().unwrap().len());
+
+    let chars = input.chars().filter(|c| *c != '\n');
+    Array::from_iter(chars).into_shape(shape).unwrap()
 }
