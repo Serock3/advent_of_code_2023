46e8e78be4f814b5587edd4d8fbed3ea1843bd49
Sebastian <sebastian.holmin@hotmail.com>
2023-12-27 00:01:26 +0100

    Refactor

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 use std::collections::HashSet;
 use std::f32::consts::E;
+use std::ops::Add;
 
 use advent_of_code::get_input;
 use advent_of_code::Direction;
@@ -20,6 +21,33 @@ fn main() {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Pos<T>(T, T);
+
+impl<T: Add<Output = T>> Add for Pos<T> {
+    type Output = Pos<T>;
+
+    fn add(self, rhs: Pos<T>) -> Pos<T> {
+        Pos(self.0 + rhs.0, self.1 +rhs.1)
+    }
+}
+
+impl Add<Pos<isize>> for Pos<usize> {
+    type Output = Pos<usize>;
+
+    fn add(self, rhs: Pos<isize>) -> Pos<usize> {
+        Pos(self.0.saturating_add_signed(rhs.0) , self.1.saturating_add_signed(rhs.1) )
+    }
+}
+
+impl From<&Direction> for Pos<isize> {
+    fn from(value: &Direction) -> Self {
+        match value {
+            North => Pos(-1, 0),
+            East => Pos(0, 1),
+            South => Pos(1, 0),
+            West => Pos(0, -1),
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Beam {
@@ -65,9 +93,10 @@ fn handle_beam(
     Option<impl Iterator<Item = Beam>>,
     SliceInfo<[SliceInfoElem; 2], Dim<[usize; 2]>, Dim<[usize; 1]>>,
 ) {
+    let pos = &inbound_beam.start;
     match inbound_beam.dir {
         North => {
-            let mut beam_area = s![..inbound_beam.start.0, inbound_beam.start.1];
+            let mut beam_area = s![..=pos.0, pos.1];
 
             let maybe_new_beams = matrix
                 .slice(beam_area)
@@ -75,57 +104,58 @@ fn handle_beam(
                 .rev()
                 .find_position(|&&c| c != '.')
                 .map(|(offset, c)| {
-                    let start_row = inbound_beam.start.0 - offset;
-                    beam_area = s![start_row..inbound_beam.start.0, inbound_beam.start.1];
-                    let mirror_pos = Pos(start_row, inbound_beam.start.1);
+                    
+                    let start_row = pos.0 - offset;
+                    beam_area = s![start_row..=pos.0, pos.1];
+                    let mirror_pos = Pos(start_row, pos.1);
 
                     new_beams(inbound_beam.dir, c, mirror_pos)
                 });
             (maybe_new_beams, beam_area)
         }
         East => {
-            let mut beam_area = s![inbound_beam.start.0, inbound_beam.start.1..];
-
+            let mut beam_area = s![pos.0, pos.1..];
             let maybe_new_beams = matrix
                 .slice(beam_area)
                 .iter()
                 .find_position(|&&c| c != '.')
                 .map(|(offset, c)| {
-                    let end_col = inbound_beam.start.1 + offset;
-                    beam_area = s![inbound_beam.start.0, inbound_beam.start.1..end_col];
-                    let mirror_pos = Pos(inbound_beam.start.0, end_col);
+                    let end_col = pos.1 + offset;
+                    beam_area = s![pos.0, pos.1..=end_col];
+                    let mirror_pos = Pos(pos.0, end_col);
 
                     new_beams(inbound_beam.dir, c, mirror_pos)
                 });
             (maybe_new_beams, beam_area)
         }
         South => {
-            let mut beam_area = s![inbound_beam.start.0.., inbound_beam.start.1];
+            let mut beam_area = s![pos.0.., pos.1];
 
             let maybe_new_beams = matrix
                 .slice(beam_area)
                 .iter()
                 .find_position(|&&c| c != '.')
                 .map(|(offset, c)| {
-                    let end_row = inbound_beam.start.0 + offset;
-                    beam_area = s![inbound_beam.start.0..end_row, inbound_beam.start.1];
-                    let mirror_pos = Pos(end_row, inbound_beam.start.1);
+                    let end_row = pos.0 + offset;
+                    beam_area = s![pos.0..=end_row, pos.1];
+                    let mirror_pos = Pos(end_row, pos.1);
 
                     new_beams(inbound_beam.dir, c, mirror_pos)
                 });
             (maybe_new_beams, beam_area)
         }
         West => {
-            let mut beam_area = s![inbound_beam.start.0, inbound_beam.start.1..];
+            let mut beam_area = s![pos.0, ..=pos.1];
 
             let maybe_new_beams = matrix
                 .slice(beam_area)
                 .iter()
+                .rev()
                 .find_position(|&&c| c != '.')
                 .map(|(offset, c)| {
-                    let start_col = inbound_beam.start.1 - offset;
-                    beam_area = s![inbound_beam.start.0, start_col..inbound_beam.start.1];
-                    let mirror_pos = Pos(inbound_beam.start.0, start_col);
+                    let start_col = pos.1 - offset;
+                    beam_area = s![pos.0, start_col..=pos.1];
+                    let mirror_pos = Pos(pos.0, start_col);
 
                     new_beams(inbound_beam.dir, c, mirror_pos)
                 });
@@ -141,7 +171,7 @@ fn new_beams(
 ) -> impl Iterator<Item = Beam> {
     let next_dirs: &[Direction] = get_reflection_dir(inbound_dir, *c);
     next_dirs.iter().cloned().map(move |dir| Beam {
-        start: mirror_pos.clone(),
+        start: mirror_pos.clone() + Pos::from(&dir),
         dir,
     })
 }
